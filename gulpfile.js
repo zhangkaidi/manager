48d013c5597afcbb9d4ea91eb7794eee352c34ad
zhangkaidi <490151956@qq.com>
2017-08-25 10:14:41 +0800

    图片合并

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
 	less = require('gulp-less'),//less
     cssmin = require('gulp-minify-css'),//压缩css
+    csslint = require('gulp-csslint'),
     concat = require('gulp-concat'),
     watch = require('gulp-watch'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
-var htmlPath = './view/*.html';
+    browserSync = require('browser-sync').create(),
+    spritesmith= require('gulp.spritesmith');
     //csso = require('gulp-csso'); //合并css属性
 gulp.task('css',function () {
  	gulp.src('./src/css/*.less') //多个文件以数组形式传入
@@ -17,19 +18,33 @@ gulp.task('css',function () {
 		.pipe(cssmin()) //压缩css
 		.pipe(browserSync.reload({stream: true}));
 });
+gulp.task('img', function () {
+    gulp.src('./src/images/*.png')//需要合并的图片地址
+        .pipe(spritesmith({
+            imgName: 'sprite.png',//保存合并后图片的地址
+            cssName: 'sprite.css',//保存合并后对于css样式的地址
+            padding:5,//合并时两个图片的间距
+            algorithm: 'binary-tree',//注释1
+            cssTemplate:"./src/css/handlebarsStr.css"//注释2
+        }))
+        .pipe(gulp.dest('./dist/images'))
+        .pipe(browserSync.reload({stream: true}));
+});
 gulp.task('fileWatch', function () {
-	  var files = [
+	var files = [
         './view/*.html',
         './src/css/*.less',
+        './src/images/*.png'
     ];
-    browserSync.init(files,{
+    browserSync.init({
         server: {
-            base: "./view",
+            base: "/"
         },
-        files: ["./view/*.html", "./src/css/*.less"],
+        files: ["./*.html", "./src/css/*.less",'./src/images/*.png'],
         logPrefix: "操作提示:"
     });
-    gulp.watch('./src/css/*.less', ['css']); 
-    gulp.watch(htmlPath).on('change', browserSync.reload);
+    gulp.watch('./src/css/*.less', ['css']);
+    gulp.watch('./src/images/*.png', ['img']); 
+    gulp.watch('/view/*.html').on('change', browserSync.reload);
 });
-gulp.task('default', ['fileWatch']);
+gulp.task('default', ['fileWatch','img','css']);
